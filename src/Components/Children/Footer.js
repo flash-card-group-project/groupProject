36b14ed3768b25140b9f7cab5a7332ff3937c98c
Erin Schull <erin.schull@gmail.com>
2017-12-06 08:21:24 -0700

    initialized footer

---
+++ src/Components/Children/Footer.js
@@ -0,0 +1,11 @@
+//erin tues 12/5
+import React, { Component } from 'react';
+
+class Footer extends Component{
+    render(){
+        return (
+            <h1>This is the footer</h1>
+        )
+    }
+}
+export default Footer
