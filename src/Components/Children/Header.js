e24cb7b2843656a80c474de6c6c5b9af8d32587c
Ana Ortiz <ana.ortiz.y.b.7@gmail.com>
2017-12-06 12:26:05 -0700

    worked on header and landing

--- src/Components/Children/Header.js
+++ src/Components/Children/Header.js
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 
 export default function Header() {
     return (
-            <div>
-                <ul className='nav_links'>
-                    <Link to='/home'><li>HOME</li></Link>
-                    <Link to='/my_decks'><li>MY DECKS</li></Link>
-                    <Link to='/favorites'><li>FAVORITES</li></Link>
-                    <a href={process.env.REACT_APP_LOGOUT}><p>LOG OUT</p></a>
-                </ul>
-            </div>
+            <section>
+                <div className='nav_links'>
+                    <Link to='/home'><div>HOME</div></Link>
+                    <Link to='/my_decks'><div>MY DECKS</div></Link>
+                    <Link to='/favorites'><div>FAVORITES</div></Link>
+                    <a href={process.env.REACT_APP_LOGOUT}><div>LOG OUT</div></a>
+                </div>
+            </section>
     )
 }
--- src/Components/View/Landing.js
+++ src/Components/View/Landing.js
@@ -1,12 +1,34 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-class Landing extends Component{
-    render(){
+
+class Landing extends Component {
+    render() {
+
         return (
-            <div>
-            <div>I am the landing page</div>
-            <a href={process.env.REACT_APP_LOGIN}><button>login</button></a>
-            </div>
+            <main>
+                <section>
+                    <h1>Flash Toolkit</h1>
+                   
+                     <h2>Create,Study,Shuffle,Repeat</h2> 
+                    <a href={process.env.REACT_APP_LOGIN}><button>login</button></a>
+                </section>
+                <section>
+                    <h2>Getting Started</h2>
+                    <div>
+                        <img alt='deck_image' />
+                        <h2>Create Deck</h2>
+                        <p>Lorem ipsum will explain how to
+                      create a deck?
+                  </p>
+                        <img alt='card_image' />
+                        <h2>Create Card</h2>
+                        <p>will show how to create a card</p>
+                        <img alt='study_image' />
+                        <h2>Study</h2>
+                        <p>will show study, shuffle etc..</p>
+                    </div>
+                </section>
+            </main>
         )
     }
 }
