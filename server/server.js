37fd0d68f6caf334492bce4d3450cfeb99f6db1f
MountainGirl504 <natalia_calt@yahoo.com>
2017-12-06 14:01:04 -0700

    changed

--- server/server.js
+++ server/server.js
@@ -105,17 +105,10 @@ app.get('/auth/logout', (req, res) => {
 //Get All public Decks, need this to find category
 app.get('/api/all/decks', decksCtrl.getAllPublicDecks);
 //Parent decks
-<<<<<<< HEAD
 app.get('/api/decks', decksCtrl.allParentDecks);
 //Decks by category ==> by userInput
 // app.get(`api/decks/?q=${req.query.term}`, decksCtrl.decksByCategory);
 //Decks and subdecks?
-=======
-// app.get('/api/decks', decksCtrl.allParentDecks);
-// //Decks by category ==> by userInput
-// app.get(`api/decks/?q=${req.query.term}`, decksCtrl.decksByCategory);
-// //Decks and subdecks?
->>>>>>> 47667d7b68f5cbf1524a07d3e520eeda4ef27309
 // app.get('/api/user/decks/:creatorId', decksCtrl.userDecks);
 // //Create new Deck
 // app.post('/api/create/deck', decksCtrl.createDeck);
@@ -126,16 +119,9 @@ app.get('/api/decks', decksCtrl.allParentDecks);
 // //Get Favorites
 // app.get('/api/users/favorites/:userID', decksCtrl.getFavorites);
 // //Study decks
-<<<<<<< HEAD
 // // app.get('/api/deck/study/:deckId', decksCtrl.getStudy);
 // //Get Children???
 // app.get('/api/users/decks/:userId', decksCtrl.getSudy);
-=======
-// app.get('/api/deck/study/:deckId', decksCtrl.getStudy);
-// //Get Children???
-// app.get('/api/users/decks/:userId', decksCtrl.getSudy);
-
->>>>>>> 47667d7b68f5cbf1524a07d3e520eeda4ef27309
 
 
 
