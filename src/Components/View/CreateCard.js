9a29ed7235a37039a09937f60c73050352ed0e01
Ana Ortiz <ana.ortiz.y.b.7@gmail.com>
2017-12-07 16:12:25 -0700

    added/fixed some positioning

--- src/Components/View/CreateCard.js
+++ src/Components/View/CreateCard.js
@@ -5,15 +5,15 @@ class CreateCard extends Component {
         return (
             <main className='create_card_bod'>
                 <section className='box1'>
-                        <textarea placeholder='please enter your question here..' className='card2'/>
-                    <div>
+                    <textarea placeholder='please enter your question here..'className='card2' />
+                    <div className='btn_positioning'>
                         <button className='med_btn'>MC</button>
                         <button className='med_btn'>URL</button>
                     </div>
                 </section>
                 <section className='box1'>
-                    <textarea placeholder='please enter your answer here...' className='card2'/>
-                    <div>
+                    <textarea placeholder='please enter your answer here...'className='card2' />
+                    <div className='btn_positioning'>
                         <button className='lrg_btn'>save</button>
                     </div>
                 </section>
