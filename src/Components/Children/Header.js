13054544f034864d2efb7299645841c7d23d42bb
Ana Ortiz <ana.ortiz.y.b.7@gmail.com>
2017-12-07 08:44:21 -0700

    minor changes

--- src/Components/Children/Header.js
+++ src/Components/Children/Header.js
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 
 export default function Header() {
     return (
-            <section>
-                <div className='nav_links'>
-                    <Link to='/home'><div>HOME</div></Link>
-                    <Link to='/my-decks/:id'><div>MY DECKS</div></Link>
-                    <Link to='/favorites'><div>FAVORITES</div></Link>
-                    <a href={process.env.REACT_APP_LOGOUT}><div>LOG OUT</div></a>
-                </div>
-            </section>
+        <section>
+            <div className='nav_links'>
+                <div><Link to='/home'>HOME</Link></div>
+                <div><Link to='/my-decks/:id'>MY DECKS</Link></div>
+                <div><Link to='/favorites'>FAVORITES</Link></div>
+                <a href={process.env.REACT_APP_LOGOUT}><div>LOG OUT</div></a>
+            </div>
+        </section>
     )
 }
