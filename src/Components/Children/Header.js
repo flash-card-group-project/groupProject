d43a243cd613ec1d67266952a0a30d1c37347f6d
Ana Ortiz <ana.ortiz.y.b.7@gmail.com>
2017-12-07 10:22:11 -0700

    trying to fix the header rendering

--- src/Components/Children/Header.js
+++ src/Components/Children/Header.js
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom';
 export default function Header() {
     return (
         <section>
-            <div className='nav_links'>
+            <nav className='nav_links'>
                 <div><Link to='/home'>HOME</Link></div>
                 <div><Link to='/my-decks/:id'>MY DECKS</Link></div>
                 <div><Link to='/favorites'>FAVORITES</Link></div>
                 <a href={process.env.REACT_APP_LOGOUT}><div>LOG OUT</div></a>
-            </div>
+            </nav>
         </section>
     )
 }
--- src/router.js
+++ src/router.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route, Switch} from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import CreateCard from './Components/View/CreateCard';
 import Favorites from './Components/View/Favorites';
 import Landing from './Components/View/Landing';
@@ -8,16 +8,17 @@ import MyDecks from './Components/View/MyDecks';
 import Search from './Components/View/Search';
 import Wrapper from './Components/Children/Wrapper';
 
-export default(
+
+export default (
     <div>
         <Switch>
-            <Route exact path='/' component={Landing}/>
-            <Route path='/home' component={Home}/>
-            <Route path='/my-decks/:id' component={MyDecks}/>
-            <Route path='/favorites' component={Favorites}/>
-            <Route path='/search' component={Search}/>
-            <Route path='/create-card' component={CreateCard}/>
-            <Route path='/viewer' component={Wrapper}/>    
+            <Route exact path='/' component={Landing} />
+            <Route path='/home' component={Home} />
+            <Route path='/my-decks/:id' component={MyDecks} />
+            <Route path='/favorites' component={Favorites} />
+            <Route path='/search' component={Search} />
+            <Route path='/create-card' component={CreateCard} />
+            <Route path='/viewer' component={Wrapper} />
         </Switch>
     </div>
 )
