8e1d679b51fd4d6b61a1a5416158969a9e4c463e
MountainGirl504 <natalia_calt@yahoo.com>
2017-12-08 08:30:37 -0700

    merging

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import {Provider} from 'react-redux'
 import registerServiceWorker from './registerServiceWorker';
-import { Provider } from 'react-redux';
 import store from './ducks/store';
 
 ReactDOM.render(
