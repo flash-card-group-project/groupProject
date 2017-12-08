5e5789abec62e81a2ff5d8dbb8202a0d76a4f3d7
Mark Nielsen <marknielsen@Marks-MacBook-Pro.local>
2017-12-08 11:32:34 -0700

    wrapper and toggle button work

---
+++ src/Components/View/DeckViewerD.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class DeckViewerD extends Component {
+    constructor() {
+        super();
+        this.state = {};
+    }
+    render() {
+        return(
+            <div>DeckViewerD</div>            
+        )
+    }
+}
+
+export default DeckViewerD;
--- src/Components/View/DeckViewerM.js
+++ src/Components/View/DeckViewerM.js
@@ -1,9 +1,38 @@
 import React, { Component } from 'react';
+import ToggleButton from 'react-toggle-button';
 
 class DeckViewerM extends Component {
+    constructor(){
+        super();
+        this.state = {
+            cardView: true
+        }
+        this.toggleView = this.toggleView.bind(this);
+    }
+
+    toggleView(){
+        let currentView = this.state.cardView;
+        this.setState({
+            cardView: !currentView
+        })
+    }
+
     render() {
         return (
-            <div>I am the deck viewer for the mobile view</div>
+            <div>
+                <div>
+                    <ToggleButton
+                        value={ this.state.cardView}
+                        onToggle={this.toggleView}
+                        inactiveLabel={'Decks'}
+                        activeLabel={'Cards'}
+                         />
+                </div>
+                
+                <div>
+
+                </div>
+            </div>
         )
     }
 }
