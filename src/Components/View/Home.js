101d327fcf1be1f65476fad99a0e7308d2459df8
Ana Ortiz <ana.ortiz.y.b.7@gmail.com>
2017-12-09 16:56:12 -0700

    fixed some styling

--- src/Components/View/Home.js
+++ src/Components/View/Home.js
@@ -16,6 +16,8 @@ class Home extends Component {
         // console.log(this.props);
         return (
             <main className='home_body'>
+                <h2 className='title_sml'>Welcome, {this.props.userData.first_name}!</h2>
+                <h2>Where would you like to go? </h2>
                 <CreateDeck />
                 <Link to={`/my-decks/${this.props.userData.userId}`} className='card'>My Decks</Link>
                 <Link to={`/favorites/${this.props.userData.userId}`} className='card'>Favorites</Link>
--- src/Components/View/Landing.js
+++ src/Components/View/Landing.js
@@ -4,12 +4,12 @@ export default function Landing() {
     return (
         <main className='landing_body'>
             <section className='focal_point'>
-                <h1>Flash Toolkit</h1>
-                <h2>Create, Study, Shuffle, Repeat</h2>
+                <h1 className='title_bg'>Flash Toolkit</h1>
+                <h2 className='title_sml'>Create, Study, Shuffle, Repeat</h2>
                 <a href={process.env.REACT_APP_LOGIN}><button>Sign In / Register</button></a>
             </section>
             <section className='steps'>
-                <h2>Getting Started</h2>
+                <h2 className='title_sml'>Getting Started</h2>
                 <img  alt='deck_image' className='card'/>
                 {/* <h2>Create Deck</h2> */}
                 <p className='card'>will explain how to create a deck?</p>
