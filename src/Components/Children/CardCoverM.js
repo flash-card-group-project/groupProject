3137ffdcab17f855d5748f6dde9699977f32ba12
SecretMNC <pettkevinj@gmail.com>
2017-12-11 09:51:36 -0700

    some changes

--- src/Components/Children/CardCoverM.js
+++ src/Components/Children/CardCoverM.js
@@ -13,40 +13,41 @@ class CardCoverM extends Component {
             card_question: this.props.match.params.question,
             deleteStatus: ''
         };
+        this.handleClickDelete = this.handleClickDelete.bind(this);
     };
 
-//handleClick will give an error about confirm -Kevin 12/8
+    //handleClick will give an error about confirm -Kevin 12/8
 
-    // handleClick() {
-    //     let deleteCard = confirm('Are you sure you want to delete this question?');
-    //     if (deleteCard === true) {
-    //         axios.delete('/api/delete/card').then(response => {
-                
-    //         })
-    //     } else {
-    //         this.setState({
-    //             deleteStatus: 'Question was not deleted!'
-    //         })
-    //     }
-    // }
+    handleClickDelete() {
+        let deleteCard = confirm('Are you sure you want to delete this question?');
+        if (deleteCard === true) {
+            axios.delete('/api/delete/card').then(response => {
+
+            })
+        } else {
+            this.setState({
+                deleteStatus: 'Question was not deleted!'
+            })
+        }
+    }
 
     render() {
         return (
             <div>
-                <p style={{color: 'red'}}>{this.state.deleteStatus}</p>
+                <p style={{ color: 'red' }}>{this.state.deleteStatus}</p>
                 <div>
                     {this.state.card_question}
                 </div>
                 <Link to='/viewer'>
-                <button>
-                    <img src='' alt='Edit' />
-                </button>
+                    <button>
+                        <img src='' alt='Edit' />
+                    </button>
                 </Link>
-                
-                <button>
+
+                <button onClick={this.handleClickDelete}>
                     <img src='' alt='Delete' />
                 </button>
-                
+
             </div>
         )
     }
