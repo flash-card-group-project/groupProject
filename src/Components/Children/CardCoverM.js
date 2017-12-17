744770f56d05ee86da6fc049de42d7750c78c95b
Ana Ortiz <ana.ortiz.y.b.7@gmail.com>
2017-12-17 13:45:36 -0700

    styling

--- src/Components/Children/CardCoverM.js
+++ src/Components/Children/CardCoverM.js
@@ -6,31 +6,15 @@ import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { getDecksHome, deleteCard } from '../../ducks/reducer';
-import editIcon from '../Assets/editing.png';
 import trashCan from '../Assets/deleteIcon.png';
 
 class CardCoverM extends Component {
 
     constructor(props) {
         super(props)
-
-        this.state = {
-            deleteStatus: ''
-        };
         this.handleClickDelete = this.handleClickDelete.bind(this);
     };
-    // componentDidMount(){
-    //     console.log("it gets HERE")
-    //     this.props.getDecksHome();
-        
-    // }
-    // componentWillUpdate(){
 
-    // }
-    // //handleClick
-    // componentWillMount(){
-    //     this.props.getDecksHome();
-    // }
     handleClickDelete() {
         let deleteCard = confirm('Are you sure you want to delete this card?');
         if (deleteCard === true) {
@@ -50,12 +34,6 @@ class CardCoverM extends Component {
                 </Link>
 
                 <div className="card-buttons">
-                    <div className="card-cover-buttons">
-                        <button>
-                            <img src={editIcon} alt='Edit' />
-                        </button>
-                    </div>
-
                     <div className="card-cover-buttons">
                         <button onClick={this.handleClickDelete}>
                             <img src={trashCan} alt='Delete' />
